d7548cf63855ee04631731cc57caa1a9d471b531
EmiliaGuz <64081262+EmiliaGuz@users.noreply.github.com>
2020-05-05 10:42:51 -0500

    Update 5.15.cpp

--- 5.15/5.15/5.15.cpp
+++ 5.15/5.15/5.15.cpp
@@ -1,37 +1,68 @@
 #include <iostream>
-
 using namespace std;
+
+
 
 int main()
 {
     int n = 10;
-    for (int i = 1; i <= n; i++)
+
+    cout << "Pattern 1: " << endl;
+
+    for (int i = 1; i <= n; i++)//Salto de linea horizontal
     {
-        for (int j = 40; j > n * 2; j++)
+        for (int j = 1; j <= i; j++)//dibuja asteriso
         {
-            if (j <= i || j >= n * 2 -1) {
-                cout << "*";
-            }
-            else {
-                cout << " ";
-            }
+            cout << "*";
         }
         cout << endl;
     }
-    int px = n;
-    int py = n;
-    for (int x = 11; x >= n; x++) {
-        for (int y = 11; y > n * 2; y++) {
-            if (y > px && y < py) {
-                cout << " ";
-            }
-            else {
-                cout << "*";
-            }
+    
+    cout << '\n';
+
+    cout << "Pattern 2: " << endl;
+
+    for (int i = 10; i >= 0; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
         }
-        px--;
-        py++;
         cout << endl;
     }
-    return 0;
+ 
+    cout << '\n';
+
+    cout << "Pattern 3: " << endl;
+
+    for (int x = 10; x >= 1; x--)
+    {
+        for (int y = n - 1; y >= x; y--)
+        {
+            cout << " ";
+        }
+        for (int w = 1; w <= x; w++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << '\n';
+
+    cout << "Pattern 4: " << endl;
+
+    for (int x = 1; x <= n; x++)
+    {
+        for (int y = n - 1; y >= x; y--)
+        {
+            cout << " ";
+        }
+        for (int w = 1; w <= x; w++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
 }
