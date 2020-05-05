28f8e687a40969ccf9d270bfc9caf911c8ead42a
EmiliaGuz <64081262+EmiliaGuz@users.noreply.github.com>
2020-05-05 07:46:35 -0500

    Update 4.30.cpp

--- 4.30/4.30/4.30.cpp
+++ 4.30/4.30/4.30.cpp
@@ -5,10 +5,10 @@ int main()
 {
     int numero, n1=0, n2=1;
     int n,i=0;
-    cout << "Introduce numero : ";
-    cin >> numero;
+    //cout << "Introduce numero : ";
+    //cin >> numero;
     cout << "Es la cantidad de elementos en la serie fibonacci)
-    while(i<numero) {
+    while(true) {
         n = n2;
         n2 += n1;//Intercambio de variable
         n1 = n;
